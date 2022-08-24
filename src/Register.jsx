fea7b54e663fe4343ae5230ed766550abda4917f
YUYING0327 <105634108+YUYING0327@users.noreply.github.com>
2022-08-24 09:09:08 +0800

    ref、state、useEffect and validation logic

--- src/Register.jsx
+++ src/Register.jsx
@@ -6,10 +6,57 @@ import {
 } from '@fontawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+//  正則表達式 (RegExp)
+//  以小寫或大寫字母開頭，包含 2 - 23 個可以是大、小寫英文字母或數字，總體長度為 4 - 24
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3, 23}$/;
+//  至少需要一個小寫字母、大寫字母、數字和特殊符號，可以是 8 - 24 字符之間
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8, 24}$/;
 
 const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  //   檢查 user 是否匹配
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  //  檢查 PWD 是否匹配
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd, matchPwd]);
+
   return <div>Register</div>;
 };
 
